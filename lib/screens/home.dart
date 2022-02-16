105144e269f9a40bcc3cc0b214bea0030210b653
Darius <68332672+Darius253@users.noreply.github.com>
2022-02-16 21:57:21 +0000

    Added homeppage file

---
+++ lib/screens/home.dart
@@ -0,0 +1,22 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:win/widgets/appbar.dart';
+import 'package:win/widgets/mainScreen.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        appbar(),
+        const MainScreeen(),
+       ],
+    );
+  }
+}
