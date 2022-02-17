172c20f431b4ed267b28d1c5c512504435ba571d
Darius <68332672+Darius253@users.noreply.github.com>
2022-02-17 16:22:03 +0000

    Code Refactor

--- lib/widgets/mainScreen.dart
+++ lib/widgets/mainScreen.dart
@@ -1,4 +1,8 @@
+
+
+
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:win/widgets/sidemenu.dart';
 
 class MainScreeen extends StatefulWidget {
   const MainScreeen({Key? key}) : super(key: key);
@@ -9,18 +13,23 @@ class MainScreeen extends StatefulWidget {
 
 class _MainScreeenState extends State<MainScreeen> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return Container(height: 500, width: 100, color: Colors.blue);
-            }),
-        const SizedBox(
-          width: 10,
-        ),
-        Container(height: 500, width: 500, color: Colors.blue)
+        const SideMenu(),
+        
+        Expanded(
+          child: Container(
+            color: Colors.grey,
+            child: const Center(child:  Text("HomePage")),
+          ),
+        )
       ],
     );
   }
