5f9331d0c86ac8d11e4aa2999abcee7b1eb094bd
Darius <68332672+Darius253@users.noreply.github.com>
2022-03-02 23:43:11 +0000

    Added few items to expander tiles

--- lib/widgets/expander.dart
+++ lib/widgets/expander.dart
@@ -2,54 +2,119 @@ import 'package:fluent_ui/fluent_ui.dart';
 
 Widget expanderItems() {
   return Column(children: [
-    const Expander(
-      leading: Icon(FluentIcons.home_solid),
-      header: Text('Home'),
-      content: Text('More'),
+    GestureDetector(
+      child: const ListTile(
+        leading: Icon(FluentIcons.home_solid),
+        title: Text('Home'),
+      ),
+    ),
+    GestureDetector(
+      child: const ListTile(
+        leading: Icon(FluentIcons.dashboard_add),
+        title: Text('Dashboard'),
+      ),
+    ),
+    GestureDetector(
+      child: const ListTile(
+        leading: Icon(FluentIcons.mail),
+        title: Text('Messages'),
+      ),
+    ),
+    
+    // Accountant  Expansion Tile
+
+
+    Expander(
+      leading: const Icon(FluentIcons.circle_dollar),
+      header: const Text('Accountant'),
+      content: Column(
+        children: [
+          GestureDetector(
+              child: const ListTile(
+            title: Text('Account Center'),
+          )),
+          GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Batch Enter Transactions')],),
+              )),
+              GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Journals')],),
+              )),
+              GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Make General Journal Entries')],),
+              )),
+              GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Chart of Accounts')],),
+              )),
+              GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Fixed Asset Item')],),
+              )),
+              GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Send General Journal Entries')],),
+              )),
+              GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Manage FixedAssets')],),
+              )),
+              GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Reconcile')],),
+              )),
+              GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Working Trial Balance')],),
+              )),
+              //TreeView(items: [
+                ////TreeViewItem(content: GestureDetector(child: const ListTile(title: Text('Fixed Assets List'),),))
+             // ],)
+        ],
+      ),
       direction: ExpanderDirection
           .down, // (optional). Defaults to ExpanderDirection.down
       initiallyExpanded: false, // (false). Defaults to false
     ),
-    const Expander(
-      leading: Icon(FluentIcons.dashboard_add),
-      header: Text('Dashboard'),
-      content: Text('More'),
+     
+
+    // Company Expander Tile
+
+
+     Expander(
+      leading: const Icon(FluentIcons.bank),
+      header: const Text('Company'),
+      content: Column (
+        children: [
+          GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Calender')],),
+              )),
+              GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Documents')],),
+              )),
+              GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('Reminders')],),
+              )),
+              GestureDetector(
+              child:  ListTile(
+            title: Wrap (children:const [ Text('To do List')],),
+              )),
+        ],
+      ),
       direction: ExpanderDirection
           .down, // (optional). Defaults to ExpanderDirection.down
       initiallyExpanded: false, // (false). Defaults to false
     ),
-    const Expander(
-      leading: Icon(FluentIcons.mail),
-      header: Text('Messages'),
-      content: Text('More'),
-      direction: ExpanderDirection
-          .down, // (optional). Defaults to ExpanderDirection.down
-      initiallyExpanded: false, // (false). Defaults to false
-    ),
-    const Expander(
-      leading: Icon(FluentIcons.circle_dollar),
-      header: Text('Accountant'),
-      content: Text('More'),
-      direction: ExpanderDirection
-          .down, // (optional). Defaults to ExpanderDirection.down
-      initiallyExpanded: false, // (false). Defaults to false
-    ),
-    const Expander(
-      leading: Icon(FluentIcons.bank),
-      header: Text('Company'),
-      content: Text('More'),
-      direction: ExpanderDirection
-          .down, // (optional). Defaults to ExpanderDirection.down
-      initiallyExpanded: false, // (false). Defaults to false
-    ),
-    const Expander(
-      leading: Icon(FluentIcons.asset_library),
-      header: Text('Assests'),
-      content: Text('More'),
-      direction: ExpanderDirection
-          .down, // (optional). Defaults to ExpanderDirection.down
-      initiallyExpanded: false, // (false). Defaults to false
-    ),
+
+    // Customers Expander Tile
+
+
+
     Expander(
       leading: const Icon(FluentIcons.circle_dollar),
       header: const Text('Customers'),
@@ -66,6 +131,25 @@ Widget expanderItems() {
         ListTile(
           title: Text('Revenues'),
         ),
+        ListTile(
+          title: Text('Estimates'),
+        ),
+        ListTile(
+          title: Text('Sales Orders'),
+        ),
+        ListTile(
+          title: Text('Statement Changes'),
+        ),
+        ListTile(
+          title: Text('Received Payments'),
+        ),
+        ListTile(
+          title: Text('Credit Memos'),
+        ),
+        ListTile(
+          title: Text('Refunds'),
+        ),
+        
       ]),
       direction: ExpanderDirection
           .down, // (optional). Defaults to ExpanderDirection.down
