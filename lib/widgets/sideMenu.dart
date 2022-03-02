7a18e856975b52f6b090a403c06924a27569219a
Darius <68332672+Darius253@users.noreply.github.com>
2022-03-02 23:43:36 +0000

    Removed unwanted code

--- lib/widgets/sideMenu.dart
+++ lib/widgets/sideMenu.dart
@@ -11,77 +11,80 @@ class SideMenu extends StatefulWidget {
 class _SideMenuState extends State<SideMenu> {
   final ScrollController _controller = ScrollController();
 
-  final values = ['Default', 'Change'];
+  final values = ['Default', 'Top', 'Bottom'];
   String? comboBoxValue;
   bool _checked = true;
   @override
   void initState() {
     super.initState();
-    _controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromARGB(255, 160, 241, 201),
-      height: 100,
-      width: 250,
-      padding: const EdgeInsets.only(top: 10),
-      child: Scrollbar(
-        controller: _controller,
-        isAlwaysShown: true,
-        child: SingleChildScrollView(
-            controller: _controller,
-            child: Column(
-              children: [
-                SizedBox(
-                  width: 150,
-                  child: Combobox<String>(
-                    comboboxColor: const Color.fromARGB(255, 160, 241, 201),
-                    placeholder: const Text('Default'),
-                    isExpanded: true,
-                    items: values
-                        .map((e) => ComboboxItem<String>(
-                              value: e,
-                              child: Text(e),
-                            ))
-                        .toList(),
-                    value: comboBoxValue,
-                    onChanged: (value) {
-                      // print(value);
-                      if (value != null) setState(() => comboBoxValue = value);
-                    },
+    return SingleChildScrollView(
+      child: Container(
+        color: const Color.fromARGB(255, 213, 236, 225),
+        width: 350,
+        padding: const EdgeInsets.only(top: 10),
+        child: Scrollbar(
+          controller: _controller,
+          isAlwaysShown: true,
+          child: SingleChildScrollView(
+              controller: _controller,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Combobox<String>(
+                      comboboxColor: const Color.fromARGB(255, 160, 241, 201),
+                      placeholder: const Text('Default'),
+                      isExpanded: true,
+                      items: values
+                          .map((e) => ComboboxItem<String>(
+                                value: e,
+                                child: Text(e),
+                              ))
+                          .toList(),
+                      value: comboBoxValue,
+                      onChanged: (value) {
+                        // print(value);
+                        if (value != null) {
+                          setState(() => comboBoxValue = value);
+                        }
+                      },
+                    ),
                   ),
-                ),
-                Row(
-                  children: [
-                    const SizedBox(
-                      width: 40,
-                    ),
-                    Checkbox(
-                      checked: _checked,
-                      onChanged: (value) => setState(() => _checked = value!),
-                    ),
-                    const Text(
-                      'Top',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    const SizedBox(
-                      width: 60,
-                    ),
-                    const Text(
-                      'Bot',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    Checkbox(
-                      checked: !_checked,
-                      onChanged: (value) => setState(() => _checked = !value!),
-                    ),
-                  ],
-                ),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 40,
+                      ),
+                      Checkbox(
+                        checked: _checked,
+                        onChanged: (value) => setState(() => _checked = value!),
+                      ),
+                      const Text(
+                        'Top',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      const SizedBox(
+                        width: 60,
+                      ),
+                      const Text(
+                        'Bot',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Checkbox(
+                        checked: !_checked,
+                        onChanged: (value) =>
+                            setState(() => _checked = !value!),
+                      ),
+                    ],
+                  ),
                   expanderItems()
-              ],
-            )),
+                ],
+              )),
+        ),
       ),
     );
   }
